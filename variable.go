e86196b4e4e9f8559cc659a4afb5f1c7b58ef765
Firman Zain <firman@paydia.id>
2024-01-25 23:09:37 +0700

    init code

---
+++ variable.go
@@ -0,0 +1,18 @@
+package main
+import "fmt"
+
+func main()  {
+	name := "Firman"
+	fmt.Println(name)
+
+	name = "Firman Zain"
+	fmt.Println(name)
+
+	var (
+		firstName = "Firman"
+		lastName = "Zain"
+	)
+
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+}
